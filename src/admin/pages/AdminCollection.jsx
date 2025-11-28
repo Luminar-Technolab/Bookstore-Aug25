a59dd9225535701dc26c7159c8fa4524c4f8fc2d
soniagkalliath <soniageorge111@gmail.com>
2025-11-28 15:34:48 +0530

    design -a dmin complete

--- src/admin/pages/AdminCollection.jsx
+++ src/admin/pages/AdminCollection.jsx
@@ -1,8 +1,60 @@
-import React from 'react'
+import React, { useState } from 'react'
+import AdminHeader from '../components/AdminHeader'
+import Footer from '../../components/Footer'
+import AdminSidebar from '../components/AdminSidebar'
 
 function AdminCollection() {
+  const [tab,setTab] = useState(1)
   return (
-    <div>AdminCollection</div>
+    <>
+    <AdminHeader/>
+    <div className='md:grid grid-cols-5'>
+      <div className="col-span-1">
+        <AdminSidebar/>
+      </div>
+      <div className="col-span-4 p-10">
+        <h1 className='text-center my-5 font-bold text-3xl'>All Resources</h1>
+        <div className="flex justify-center my-10 text-xl font-semibold">
+          <p onClick={()=>setTab(1)} className={tab==1?'text-blue-600 border-gray-300 p-3 border-t border-l border-r cursor-pointer':' border-gray-300 border-b p-2'}>Books</p>
+          <p  onClick={()=>setTab(2)} className={tab==2?'text-blue-600 border-gray-300 p-3 border-t border-l border-r cursor-pointer':' border-gray-300 border-b p-2'}>Users</p>
+        </div>
+        {
+          tab==1 &&
+          <div className='md:grid grid-cols-4 w-full my-5'>
+            {/* duplicate book card */}
+            <div className="shadow rounded p-3 m-4  md:my-0">
+            <img width={'100%'} height={'300px'} src="https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1654371463i/18144590.jpg" alt="book" />
+            <div className="flex flex-col justify-center items-center mt-4">
+              <h3 className="text-blue-700 font-bold text-xl">Author</h3>
+              <p className='text-lg'>title</p>
+              <p>$ 12 </p>
+              <button className="bg-green-600 text-white p-2 mt-2 w-full">APPROVE</button>
+            </div>
+          </div>
+          
+          </div>
+          
+        }
+
+        { tab==2 &&
+          <div className='md:grid grid-cols-3 w-full my-5'>
+            {/* duplicate user card */}
+            <div className="rounded bg-gray-200 p-2 m-2">
+              <p className="text-red-600 font-bold text-md">ID : 6325432-12931-9</p>
+              <div className="flex items-center mt-3">
+                <img width={'80px'} height={'80px'} style={{borderRadius:'50%'}} src="https://img.freepik.com/premium-photo/happy-man-ai-generated-portrait-user-profile_1119669-1.jpg" alt="user" />
+                <div className="flex flex-col ml-3 w-full">
+                    <h4 className="text-blue-600 font-bold text-lg">username</h4>
+                    <p>email</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        }
+      </div>
+    </div>
+    <Footer/>
+    </>
   )
 }
 
--- src/admin/pages/AdminProfile.jsx
+++ src/admin/pages/AdminProfile.jsx
@@ -1,8 +1,79 @@
 import React from 'react'
-
+import AdminHeader from '../components/AdminHeader'
+import Footer from '../../components/Footer'
+import AdminSidebar from '../components/AdminSidebar'
+import { FaPen } from 'react-icons/fa'
+import { FaX } from 'react-icons/fa6'
 function AdminProfile() {
   return (
-    <div>AdminProfile</div>
+    <>
+    <AdminHeader/>
+    <div className='md:grid grid-cols-5'>
+        <div className="col-span-1">
+          <AdminSidebar/>
+        </div>
+        <div className='col-span-4 p-10'>
+          
+          <h1 className="text-center mb-10 font-bold text-3xl">Settings</h1>
+          <div className="md:grid grid-cols-2 items-center">
+            <div>
+                <p className='text-xl mb-5'>Welcome, Admin 👋</p>
+                <p className='text-justify'>
+                  This is your personal administration space where you can manage your account details, system preferences, and platform roles with ease.
+                  
+                  From here, you can update essential information such as your username, password, contact details, and notification preferences — ensuring your access remains secure and personalized.
+                </p>
+
+                <p className='text-lg my-5'>🔧 What You Can Manage in This Section:</p>
+
+                <ul>
+                  <li>✏️ Update personal details (name, email, role, profile picture)</li>
+
+                <li>🔐 Change or reset your password</li>
+
+                <li>📢 Configure notification and alert preferences</li>
+
+                <li>👥 Manage permissions based on assigned access level</li>
+
+                <li>🧩 Customize dashboard visibility and layout</li>
+                </ul>
+
+
+
+                <p className='text-justify my-5'>
+                  Your profile settings help ensure your administrative tools work the way you need them to — securely, efficiently, and with complete control.
+                  
+                  Thank you for keeping the platform organized and running smoothly.
+                  Continue managing, updating, and improving the system — one step at a time. 🚀📚
+                </p>
+            </div>
+            <div className="flex justify-center items-center flex-col m-10   bg-blue-100 p-5 rounded">
+                  <label htmlFor="userProfile">
+                    <input type="file" id='userProfile' hidden />
+                    <img className='z-52' style={{width:'150px',height:'150px',borderRadius:'50%'}} src="https://img.freepik.com/premium-photo/happy-man-ai-generated-portrait-user-profile_1119669-1.jpg" alt="profile" />
+                    <button className="bg-blue-300 z-53  text-white py-2 px-3 rounded" style={{marginLeft:'75px',marginTop:'-20px'}}><FaPen/></button>
+                  </label>
+                  <div className="mt-10 mb-3 w-full px-5">
+                    <input type="text" placeholder='Username' className="w-full   p-2 rounded bg-white" />
+                  </div>
+                  <div className=" mb-3 w-full px-5">
+                    <input type="password" placeholder='New Password' className="w-full bg-white p-2 rounded" />
+                  </div>
+                  <div className=" mb-3 w-full px-5">
+                    <input type="password" placeholder='Confirm Password' className="w-full bg-white p-2 rounded" />
+                  </div>
+                  
+                  <div className="flex justify-end  w-full px-5 mt-5">
+                      <button className="bg-yellow-600 text-white px-3 py-2 rounded">RESET</button>
+                      <button className="bg-green-600 ms-5 text-white px-3 py-2 rounded">UPDATE</button>
+                  </div>
+              </div>
+          </div>
+
+        </div>
+    </div>
+    <Footer/>
+    </>
   )
 }
 
